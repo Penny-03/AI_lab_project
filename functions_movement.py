2675c564eca7b41097e03b9f8222de11d5e09556
francyciny <131877107+francyciny@users.noreply.github.com>
2024-05-30 15:32:54 +0200

    Update functions_movement.py

--- functions_movement.py
+++ functions_movement.py
@@ -17,17 +17,21 @@ def is_click(fingertips, click_threshold, last_click_time, click_cooldown):
         return True
     return False
 
+#function for verifying if we are entering mouse mode 
+def is_mouse(finger_tips):
+    return calculate_distance(fingertips[0], fingertips[2])<0.05
+    
 #function for moving mouse
-def move_mouse():
-  if is_mouse():
+def move_mouse(finger_tips):
+  if is_mouse(finger_tips):
     screen_width, screen_height = pyautogui.size() 
-    index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+    index_tip = finger_tips[1]
     h, w, _ = image.shape
-    cx, cy = int(index_finger_tip.x * w), int(index_finger_tip.y * h)
+    cx, cy = int(index_tip.x * w), int(index_tip.y * h)
     
     # Converti le coordinate in base alla risoluzione dello schermo.
-    screen_x = int(index_finger_tip.x * screen_width)
-    screen_y = int(index_finger_tip.y * screen_height)
+    screen_x = int(index_tip.x * screen_width)
+    screen_y = int(index_tip.y * screen_height)
     
     # Muovi il mouse.
     pyautogui.moveTo(screen_x, screen_y)
@@ -37,26 +41,8 @@ def move_mouse():
 
 
 
-# Ottieni la risoluzione dello schermo.
-screen_width, screen_height = pyautogui.size()
 
 
 
-
-
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            
-            
-
-    # Mostra l'immagine con le annotazioni.
-    cv2.imshow('Hand Tracking', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
 
 
